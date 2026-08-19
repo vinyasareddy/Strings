2ddfad6e5245c6249b87f08a54557d8e20307a80
vinyasareddy <vinyasareddy761@gmail.com>
2026-08-19 22:28:10 +0530

    initial commit

---
+++ leetcode problem 1021.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int count=0;
+        string ans="";
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='('){
+                if(count>0){
+                    ans+=s[i];
+                }
+                count++;
+            }
+            else{
+                count--;
+                if(count>0){
+                    ans+=s[i];
+                }
+            }
+        }
+        return ans;
+        
+    }
+};
